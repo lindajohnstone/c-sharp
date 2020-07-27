93465a311b9b8af01ae7fedd20b28f40aa5b34fa
Linda Johnstone <linda.johnstone@myob.com>
2020-07-27 17:38:13 +1000

    Test to see if .gitignore works

---
+++ magicYrCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace magicYrCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
