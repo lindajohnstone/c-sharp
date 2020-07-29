d77e543e5fc0a3d1a9ac1a29e408ee2cf6fcddd6
Linda Johnstone <linda.johnstone@myob.com>
2020-07-29 17:15:15 +1000

    Wednesday 29th July changes

--- magicYrCalc/Program.cs
+++ magicYrCalc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace magicYrCalc
 {
@@ -6,9 +7,59 @@ namespace magicYrCalc
     {
         static void Main(string[] args)
         {
-            inputDetails(); 
+            //inputDetails(); 
+             // Create objects by using the new operator:
+
+            Console.WriteLine("Please input your name: ");
+            var PersonName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Console.ReadLine());
+            //name = (name);
+            //Console.WriteLine( "\"{0}\" to titlecase: {1}", myString, myTI.ToTitleCase( myString ) )
+            Console.WriteLine("Please input your surname:");
+            var PersonSurname = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Console.ReadLine());
+            //var fullName = name + ' ' + surname;
+            Console.WriteLine("Please enter your annual salary: ");
+            var PersonSalary = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Please enter your work start year: ");
+            //var year = int.Parse(Console.ReadLine()); does not work
+            var PersonYear = Convert.ToInt32(Console.ReadLine()); // works
+            Person person = new Person(PersonName, PersonSurname, PersonSalary, PersonYear);
+
+            // Display results:
+            //Console.Write("Child #1: ");
+            person.PrintPerson();
         }
-        
+    }
+    class Person    
+    {
+        // need new name, surname, salary & year
+            private string name;
+            private string surname;
+            private int salary;
+            private int year;
+
+            // Default constructor:
+            public Person()
+            {
+                name = "N/A";
+            }
+
+            // Constructor:
+            public Person(string name, string surname, int salary, int year)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.salary = salary;
+                this.year = year;
+            }
+
+            // Printing method:
+            public void PrintPerson()
+            {
+                Console.WriteLine("{0}, {1}", name, surname);
+                mthlySalary(salary);
+                magicYear(year);
+            }
         static void magicYear(int num)
         {
             var mgcYr = num + 65;
@@ -17,28 +68,32 @@ namespace magicYrCalc
         }
         static void mthlySalary(int num)
         {
-            var mthSalary = num / 12;
-            Console.Write("Monthly Salary: ");
-            Console.WriteLine(mthSalary);
+            var mthSalary = num / 12m; // passing number is int, is changed to decimal
+            Console.Write("Monthly Salary: $");
+            Console.WriteLine("{0:F0}",mthSalary); // diff bw Math.round == create number w/o decimal; {0:F0} == display original value w/o decimal
         } 
-        static void inputDetails()
+        /* static void inputDetails()
         {
             Console.WriteLine("Please input your name: ");
-            var name = Console.ReadLine();
+            var name = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Console.ReadLine());
+            //name = (name);
+            //Console.WriteLine( "\"{0}\" to titlecase: {1}", myString, myTI.ToTitleCase( myString ) )
             Console.WriteLine("Please input your surname:");
-            var surname = Console.ReadLine();
+            var surname = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Console.ReadLine());
             var fullName = name + ' ' + surname;
             Console.WriteLine("Please enter your annual salary: ");
             var salary = int.Parse(Console.ReadLine());
             
             Console.WriteLine("Please enter your work start year: ");
-            var year = int.Parse(Console.ReadLine());
-            //year = Convert.ToInt32(year);
-            //Convert.ToInt32(year);
+            //var year = int.Parse(Console.ReadLine()); does not work
+            var year = Convert.ToInt32(Console.ReadLine()); // works
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine(name + ' ' + surname);
             mthlySalary(salary);
             magicYear(year);
-            //Console.WriteLine(fullName);
-        }
+            Console.WriteLine();
+        } */
     }
 }
+
--- magicYrCalc/readme.txt
+++ magicYrCalc/readme.txt
@@ -8,10 +8,9 @@ Round numbers
 ** int.Parse(Console.ReadLine(());
 ** tried Math.Round - many errors!
 ** Program.cs(52,34): error CS0121: The call is ambiguous between the following methods or properties: 'Math.Round(decimal, int)' and 'Math.Round(double, int)' [/Users/Linda.Johnstone/Documents/fma/c-sharp/magicYrCalc/magicYrCalc.csproj]
-	Did not call Math.Round correctly
+	Did not call Math.Round correctly 
 ** Program.cs(54,31): error CS1503: Argument 1: cannot convert from 'method group' to 'bool' [/Users/Linda.Johnstone/Documents/fma/c-sharp/		magicYrCalc/magicYrCalc.csproj]
 	?? Don't know why
-
 
 Frustrating not knowing the correct terminology and not understanding
 it all.
@@ -22,6 +21,19 @@ Possible additions:
 ** Math.Round instead of int.Parse
 ** Uppercase first letter of name and surname if user did not input it.
 
+UPDATE 
+** successfully used Convert.ToInt32(Console.ReadLine()); - string to int to be able to be used as an integer
+** (Thanks to Simon) successfully rounded the number (code follows):
+	
+	static void mthlySalary(int num)
+        {
+            var mthSalary = num / 12m; // passing number is int, is changed to decimal
+            Console.Write("Monthly Salary: ");
+            Console.WriteLine("{0:F0}",mthSalary); // diff bw Math.round == create number 		w/o decimal; {0:F0} == display original value w/o decimal
+        } 
 
+UPDATE AGAIN
+
+** successfully used two classes instead of static class. YAY!
 
 
